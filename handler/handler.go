743b1c8b7a3d8a4d6ee3eec8767fdb3a46e97b26
fangyi <498657135@qq.com>
2019-09-17 16:19:45 +0800

    文件下载

--- handler/handler.go
+++ handler/handler.go
@@ -87,3 +87,25 @@ func FileQueryHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Write(data)
 }
+
+func DownloadHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fsha1 := r.Form.Get("filehash")
+	fm := meta.GetFileMeta(fsha1)
+	f, err := os.Open(fm.Location)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octect-stream")
+	w.Header().Set("content-disposition", "attachment;filename=\""+fm.FileName+"\"")
+	w.Write(data)
+}
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
 	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)
 	http.HandleFunc("/file/query", handler.FileQueryHandler)
+	http.HandleFunc("/file/download", handler.DownloadHandler)
 	err := http.ListenAndServe(":8989", nil)
 	if err != nil {
 		fmt.Println("Failed to start server,err:%s", err.Error())
