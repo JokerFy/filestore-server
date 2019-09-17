d54a4a56f944d0bed0c2835dfd90b16367450a67
fangyi <498657135@qq.com>
2019-09-17 15:24:41 +0800

    单文件查询

--- handler/handler.go
+++ handler/handler.go
@@ -1,11 +1,15 @@
 package handler
 
 import (
+	"encoding/json"
+	"filestore-server/meta"
+	"filestore-server/util"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,18 +30,29 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		defer file.Close()
 
-		newFile, err := os.Create("./tmp/" + head.Filename)
+		fileMeta := meta.FileMeta{
+			FileName: head.Filename,
+			Location: "./tmp/" + head.Filename,
+			UploadAt: time.Now().Format("2019-09-17 15:04:05"),
+		}
+
+		newFile, err := os.Create(fileMeta.Location)
 		if err != nil {
 			fmt.Printf("Failed to create file,err:%s\n", err.Error())
 			return
 		}
 		defer newFile.Close()
 
-		_, err = io.Copy(newFile, file)
+		fileMeta.FileSize, err = io.Copy(newFile, file)
 		if err != nil {
 			fmt.Printf("Failed to save data into file,err:%s\n", err.Error())
 			return
 		}
+
+		newFile.Seek(0, 0)
+		fileMeta.FileSha1 = util.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
+
 		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
 
@@ -45,4 +60,16 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 
 func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
 	_, _ = io.WriteString(w, "Upload finished!")
+}
+
+func GetFileMetaHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	filehash := r.Form["filehash"][0]
+	fMeta := meta.GetFileMeta(filehash)
+	data, err := json.Marshal(fMeta)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
 }
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/file/upload", handler.UploadHandler)
 	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
+	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)
 	err := http.ListenAndServe(":8989", nil)
 	if err != nil {
 		fmt.Println("Failed to start server,err:%s", err.Error())
