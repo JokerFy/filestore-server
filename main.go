7d833df6f86d71694f67b0deae71628a01193678
fangyi <498657135@qq.com>
2019-09-20 11:07:35 +0800

    接口优化

--- main.go
+++ main.go
@@ -29,81 +29,9 @@ func main() {
 	http.HandleFunc("/file/meta", ctrl.GetFileMetaHandler)
 	http.HandleFunc("/file/download", ctrl.DownloadHandler)
 
-	http.HandleFunc("/gorm/query", ctrl.Query)
 	err = http.ListenAndServe(":8989", nil)
 	if err != nil {
 		fmt.Println("Failed to start server,err:%s", err.Error())
 	}
 	fmt.Println("Server is lisenting on loacalhost:8989")
 }
-
-// 封装f：为传入的函数增加defer
-/*func errorHandler(f func()) func() {
-	return func() {
-		defer func() {
-			if r := recover(); r != nil {
-				log.Println("got error: ", r)
-			}
-		}()
-
-		f()
-	}
-}*/
-
-/*func main() {
-	test()
-}
-
-type H struct {
-	Code  int         `json:"code"`
-	Data  interface{} `json:"data"`
-	Msg   string      `json:"msg"`
-	Rows  interface{} `json:"rows,omitempty"`
-	Total interface{} `json:"total,omitempty"`
-}
-
-func test() {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Println("got error: ", r)
-		}
-	}()
-
-	var err error
-	err = a()
-	check(err)
-
-	err = b()
-	check(err)
-
-	_, err = c(1, 2)
-	check(err)
-
-	_, err = d(1, 0)
-	check(err)
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func a() error {
-	return errors.New("error in a")
-}
-
-func b() error {
-	return errors.New("error in b")
-}
-
-func c(x, y int) (int, error) {
-	return x + y, errors.New("error in c")
-}
-
-func d(x, y int) (int, error) {
-	if y == 0 {
-		return 0, errors.New("error in d, divided by 0")
-	}
-	return x / y, nil
-}*/
